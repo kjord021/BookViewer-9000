0124ae44f1bd94661ff4913f11ff301d54cc8e39
Kyle Jordan <kjord021@fiu.edu>
2021-01-21 09:38:38 -0500

    Added Individual Routing For Books To API

--- API/app.js
+++ API/app.js
@@ -15,6 +15,8 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(express.static("public"));
+
 console.log("Attempting DB Connection");
 
 mongoose.connect("mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASS+process.env.DB_LOCATION, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
@@ -42,40 +44,58 @@ const bookSchema = {
 
 const Book = mongoose.model("Book", bookSchema);
 
-//Request all books
+//Requests for all books
 
 app.route('/books')
 
-.get( function(req, res){
+  .get( function(req, res){
 
-  Book.find(  function(err, foundBooks){
-    if (!err){
-      res.send(foundBooks);
-    } else {
-      res.send(err);
-    }
+    Book.find(  function(err, foundBooks){
+      if (!err){
+        res.send(foundBooks);
+      } else {
+        res.send(err);
+      }
+    });
+  }).post( function(req, res){
+    const newBook = new Book({
+      author: req.body.author,
+      cover: req.body.cover,
+      genre: req.body.genre,
+      price: req.body.price,
+      rating: req.body.rating,
+      title: req.body.title,
+      authorBio: req.body.authorBio,
+      date: req.body.date,
+      description: req.body.description,
+      publisher: req.body.publisher
+    });
+    newBook.save(function (err) {
+      if(!err) {
+        res.send("Book added");
+      }
+      else {
+        console.log(err);
+      }
+    });
   });
-}).post( function(req, res){
-  const newBook = new Book({
-    author: req.body.author,
-    cover: req.body.cover,
-    genre: req.body.genre,
-    price: req.body.price,
-    rating: req.body.rating,
-    title: req.body.title,
-    authorBio: req.body.authorBio,
-    date: req.body.date,
-    description: req.body.description,
-    publisher: req.body.publisher
-  });
-  newBook.save(function (err) {
-    if(!err) {
-      res.send("Book added");
-    }
-    else {
-      console.log(err);
-    }
-  });
+
+//Requests for individual books
+
+app.route("/books/:bookTitle")
+
+.get( function(req, res){
+   
+  const bookTitle = req.params.bookTitle;
+
+    Book.findOne({title: bookTitle}, function(err, book){
+      if (book){
+        //const jsonBook = JSON.stringify(book);
+        res.send(book);
+      } else {
+        res.send("No book with that title found");
+      }
+    })
 });
 
 app.listen(3000, function() {
